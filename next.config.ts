790e12a0cc08da40f619ba330f9db4283e5e7a93
Ageu Assunção Simões Galindo <75844733+Bulovask@users.noreply.github.com>
2026-03-22 19:14:08 -0300

    refactor: atualizar basePath e scripts de deploy no package.json

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   
   // Opcional: Se você for hospedar no GitHub Pages (ex: seu-usuario.github.io/nome-do-repositorio/)
-  basePath: '/bitventure/out',
+  basePath: '/bitventure',
   
   // Opcional: Desativa a otimização de imagens nativa (que precisa de um servidor Node)
   images: {
